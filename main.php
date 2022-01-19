ec56a9d2b2ecfb04043c183b45ec1575b7ef6331
dedok1012 <98015051+dedok1012@users.noreply.github.com>
2022-01-19 11:16:11 +0300

    На проверку группа hacker-15

---
+++ main.php
@@ -0,0 +1,9 @@
+<div class="fullname">
+                    <p> Меня зовут
+                    <?php echo $name, ' ', $surname . '<br>';
+                          echo 'Я из города', ' ', $city; ?>
+                    </p>
+
+                    <p> Мне
+                    <?php  echo $age;   ?>
+                    года </p>
